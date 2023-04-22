2101db05dcb8355b3eb5fecf13b718c3fcf81125
Jeof Petty <jeofpetty@icloud.com>
2023-04-22 10:39:40 -0400

    stacks

--- stack.js
+++ stack.js
@@ -20,26 +20,43 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
-
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.first = newNode;
+      this.first.next = newNode;
+    }
+    this.size++;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if (this.isEmpty()) {
+      throw new Error('Stack is empty');
+    }
+    const removed = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return removed.val;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
